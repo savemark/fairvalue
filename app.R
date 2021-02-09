4c043dd2907af1c7affcc15136f40e17827b99c1
Christian Savemark <mathidler@gmail.com>
2021-02-09 19:37:24 +0100

    New app.R

---
+++ app.R
@@ -0,0 +1,14 @@
+library(shiny)
+
+# Define UI
+ui <- fluidPage(
+
+)
+
+# Define server logic
+server <- function(input, output) {
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
